ac4d13404c08f01a70fc51214ec11287062a65a5
dev_bong <bbg3813@gmail.com>
2023-10-09 16:01:28 +0900

    change all files

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -2,6 +2,7 @@ import 'package:actual/common/const/data.dart';
 import 'package:actual/common/secure_storage/secure_storage.dart';
 import 'package:actual/user/provider/auth_provider.dart';
 import 'package:dio/dio.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -21,6 +22,14 @@ final dioProvider = Provider<Dio>((ref) {
 });
 
 class CustomInterceptor extends Interceptor {
+  // onRequest가 무조건 불러진다.
+  // Req : request
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    print('[REQ] [${options.method}]');
+    return super.onRequest(options, handler);
+  }
+
+  // flutter-secure-storage 사용
   final FlutterSecureStorage storage;
   final Ref ref;
 
@@ -29,7 +38,9 @@ class CustomInterceptor extends Interceptor {
     required this.ref,
   });
 
+
   // 1) 요청을 보낼때
+
   // 요청이 보내질때마다
   // 만약에 요청의 Header에 accessToken: true라는 값이 있다면
   // 실제 토큰을 가져와서 (storage에서) authorization: bearer $token으로
